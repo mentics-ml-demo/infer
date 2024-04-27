1a8974fa9f8159e314e9153894078fea0d25e6e7
Joel Shellman <joel@mentics.com>
2024-04-27 08:12:17 -0700

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # infer
+
 Inference component. Streams input from redpanda and runs against current model.
+
+Inference can be rather slow. To horizontally scale this, the following in the main thread:
+
+1) Combine the processing time metrics into a single metric. It will be initialized to some conservative reasonable value.
+2) Periodically update the time slots on the worker threads based on the processing time metric.
+3) Maintain an events/second metric (probably EMA) on how many events occur per time slot and report this to the worker threads.
+4) It also handles model updating and telling the worker threads when to switch between models.
+
+Each thread that runs processing will target different data by doing the following:
+
+1) Maintain a processing time metric (maybe p95 or similar) for how long inference + persistence takes and report that to the main thread
+2)
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use shared_types::{Event, Inference, Inferred, Logger, StdoutLogger};
 
 // TODO: When it first comes up, check if there are enough messages in series-store in the past to fill the buffer.
 
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let reader = SeriesReader::new(Box::new(StdoutLogger()))?;
@@ -14,19 +12,8 @@ async fn main() -> anyhow::Result<()> {
 
     let predict = Predict::new(reader, store, Box::new(logger));
 
-    predict.run().await;
+    predict.run().await?;
 
-    // println!("Adding key-values");
-    // let kv1 = KeyValue { id: 1, time: 100, datum1: 11.01, datum2: 12.0 };
-    // let kv2 = KeyValue { id: 2, time: 103, datum1: 12.20, datum2: 4.2323 };
-    // let kv3 = KeyValue { id: 3, time: 107, datum1: 9.50, datum2: 3.000001111 };
-    // add_key_value(&session, kv1).await?;
-    // add_key_value(&session, kv2).await?;
-    // add_key_value(&session, kv3).await?;
-
-    // println!("Selecting key-values");
-    // let kvs = select_key_value(&session, 2).await?;
-    // println!("Found {:?}", kvs);
     Ok(())
 }
 
@@ -48,9 +35,6 @@ impl Predict {
         };
         self.reader.foreach_event(future).await;
 
-        // self.reader.foreach_event(|event| {
-        //     let res = self.on_event(event).await;
-        // });
         Ok(())
     }
 
