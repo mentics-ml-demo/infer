abcaa61385d931e1f924342d13e2f1afa4851655
Joel Shellman <joel@mentics.com>
2024-05-20 10:34:47 -0700

    Labeler in working order. Just does not handle multiple days yet.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use series_store::SeriesReader;
 use kv_store::*;
-use shared_types::{Event, Inference, Inferred, Logger, StdoutLogger};
+use shared_types::{Inference, Inferred, Logger, StdoutLogger};
 
 // TODO: When it first comes up, check if there are enough messages in series-store in the past to fill the buffer.
 
@@ -29,21 +29,21 @@ impl Predict {
     }
 
     async fn run(&self) -> anyhow::Result<()> {
-        self.reader.seek(&self.reader.topics.event, -100)?;
-        let future = move |event| async move {
-            self.on_event(event).await;
-        };
-        self.reader.foreach_event(future).await;
+        // self.reader.seek(&self.reader.topics.event, -100)?;
+        // let future = move |event| async move {
+        //     self.on_event(event).await;
+        // };
+        // self.reader.foreach_event(future).await;
 
         Ok(())
     }
 
-    async fn on_event(&self, event: Event) {
-        println!("Processing event {:?}", event);
-        // TODO: buffer
-        let inf = Inferred { id: event.id, timestamp: shared_types::now(), inference: Inference::default() };
-        let _ = self.store.write_inference(&inf).await.map_err(|e| {
-            self.logger.log(format!("Error {} inserting inferred {:?}", e, &inf));
-        });
-    }
+    // async fn on_event(&self, event: Event) {
+    //     println!("Processing event {:?}", event);
+    //     // TODO: buffer
+    //     let inf = Inferred { id: event.id, timestamp: shared_types::now(), inference: Inference::default() };
+    //     let _ = self.store.write_inference(&inf).await.map_err(|e| {
+    //         self.logger.log(format!("Error {} inserting inferred {:?}", e, &inf));
+    //     });
+    // }
 }
