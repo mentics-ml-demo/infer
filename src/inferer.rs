e8d2c50fd952838a81999269d078f5e993a7b96c
Joel Shellman <joel@mentics.com>
2024-06-11 09:41:21 -0700

    Added shared-burn repo and refactored many things to support tuple inputs to the model.

---
+++ src/inferer.rs
@@ -0,0 +1,128 @@
+use burn::{nn::loss::{MseLoss, Reduction}, tensor::{backend::Backend, Tensor}};
+
+use data_info::MODEL_OUTPUT_WIDTH;
+use shared_burn::{model::{ModelInput, ModelOutput, TheModel}, model_persist::load_model, output::print_compare_table, tensor1_to_vec, Model, TheBackend};
+use shared_types::*;
+
+pub fn make_inferer() -> anyhow::Result<Inferer<TheBackend>> {
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    Inferer::new(device)
+}
+
+pub struct Inferer<B:Backend> {
+    pub device: B::Device,
+    model: TheModel<B>,
+    loss_calc: MseLoss<B>,
+    pub metrics: OutputMetrics
+}
+
+impl<B:Backend> Inferer<B> {
+    pub fn new(device: B::Device) -> anyhow::Result<Self> {
+        let model = load_model(&device)?;
+        let loss = MseLoss::new();
+
+        println!("Is autodiff enabled for infer backend: {}", B::ad_enabled());
+        assert!(!B::ad_enabled());
+
+        Ok(Self { device, model, loss_calc: loss, metrics: OutputMetrics::default() })
+    }
+
+    // pub fn infer_and_check(&mut self, input1: ModelInput1<B>, expected: ModelOutput1<B>, display_result: bool) {
+    //     let input = (input1.0.unsqueeze(), input1.1.unsqueeze());
+    //     let output = self.infer_batch(input).squeeze(0);
+    //     // let expected = expected.to_tensor(&self.device);
+    //     let losses = self.loss_calc.forward(output.clone(), expected.clone(), Reduction::Mean);
+
+    //     if display_result {
+    //         print_compare_table(output.clone().unsqueeze(), expected.clone().unsqueeze(), &losses);
+    //     }
+
+    //     self.metrics.add(output, expected);
+
+    //     // let v = losses.into_data().convert::<f32>().value;
+    //     // assert!(v.len() == 1);
+
+    //     // v[0]
+    // }
+
+    pub fn infer_and_check(&mut self, input: ModelInput<B>, expected: ModelOutput<B>, display_result: bool) {
+        let output = self.infer_batch(input);
+        let losses = self.loss_calc.forward(output.clone(), expected.clone(), Reduction::Mean);
+
+        if display_result {
+            // print_compare_table(output.clone().unsqueeze(), expected.clone().unsqueeze(), &losses);
+            print_compare_table(output.clone(), expected.clone(), &losses);
+        }
+
+        self.metrics.add(output.squeeze(0), expected.squeeze(0));
+
+        // let v = losses.into_data().convert::<f32>().value;
+        // assert!(v.len() == 1);
+
+        // v[0]
+    }
+
+    // pub fn infer_1(&self, input1: ModelInput1<B>) -> ModelOutput1<B> {
+    //     let input = (input1.0.unsqueeze(), input1.1.unsqueeze());
+    //     let output = self.infer_batch(input).squeeze(0);
+    //     output
+    //     // output.into_data().convert::<f32>().value.try_into().map_err(|e| anyhow!("Error converting inference output to ModelOutput type {:?}", e))
+    // }
+
+    pub fn infer_batch(&self, input: ModelInput<B>) -> ModelOutput<B> {
+        self.model.forward(input)
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct OutputMetrics {
+    count: u64,
+    metrics: [Metrics; MODEL_OUTPUT_WIDTH]
+}
+
+impl OutputMetrics {
+    fn add<B:Backend>(&mut self, output: Tensor<B,1>, expected: Tensor<B,1>) {
+        let out = tensor1_to_vec(output);
+        let exp = tensor1_to_vec(expected);
+        for i in 0..MODEL_OUTPUT_WIDTH {
+            self.metrics[i].add(out[i], exp[i]);
+        }
+        self.count += 1;
+    }
+
+    pub fn print_summary(&self) {
+        let mut true_pos = 0;
+        let mut false_pos = 0;
+        for i in 0..MODEL_OUTPUT_WIDTH {
+            let m = &self.metrics[i];
+            true_pos += m.true_positives;
+            false_pos += m.false_positives;
+        }
+        println!("true_pos / count: {} / {} : {}", true_pos, self.count, true_pos as f32 / self.count as f32);
+        println!("false_pos / count: {} / {} : {}", false_pos, self.count, false_pos as f32 / self.count as f32);
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct Metrics {
+    true_positives: u64,
+    true_negatives: u64,
+    false_positives: u64,
+    false_negatives: u64,
+}
+
+impl Metrics {
+    fn add(&mut self, output: ModelFloat, actual: ModelFloat) {
+        let positive = output >= 0.5;
+        let actual_positive = actual >= 0.5;
+        if positive && actual_positive {
+            self.true_positives += 1;
+        } else if positive && !actual_positive {
+            self.false_positives += 1;
+        } else if !positive && actual_positive {
+            self.false_negatives += 1;
+        } else if !positive && !actual_positive {
+            self.true_negatives += 1;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+mod inferer;
+
 use std::{collections::{HashMap, VecDeque}, env};
 
+use convert::series_to_input;
+use model::{inputs_to_device, outputs_to_device};
 use shared_types::*;
-use series_proc::BaseHandler;
+use data_info::*;
+use series_proc::{BaseHandler, Processor};
 use series_store::*;
 use kv_store::*;
-use train::{convert::*, inferer::{make_inferer, Inferer}, TheBackend};
+use shared_burn::*;
+use quote::{QuoteEvent, QuoteValues};
+use inferer::{make_inferer, Inferer};
+
+// TODO: run metrics on true/false positive/negatives for each output element
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut count = 10;
+    let mut run_count = 10;
     // let mut reset = false;
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
@@ -20,46 +29,43 @@ async fn main() -> anyhow::Result<()> {
         }
         arg = &args[index];
         if let Ok(arg_count) = arg.parse::<usize>() {
-            count = arg_count;
+            run_count = arg_count;
         }
     }
 
     let topic = Topic::new("raw", "SPY", "quote");
-    let series = SeriesReader::new_topic(StdoutLogger::boxed(), "infer", &topic)?;
+    let series = SeriesReader::new_topic("infer", &topic)?;
     let store = KVStore::new(CURRENT_VERSION).await?;
 
-    let offset = store.next_safe_predict_offset().await?;
-    series.seek(offset - SERIES_LENGTH + 1)?;
+    let start_offset = store.next_safe_predict_offset().await?;
+    series.seek(start_offset - SERIES1_LENGTH + 1)?;
 
-    let predict = Infer::new(store, count, StdoutLogger::boxed()).await?;
-    let mut handler: BaseHandler<QuoteValues, QuoteEvent, _> = BaseHandler::new(predict);
+    let infer = Infer::new(store, start_offset, run_count).await?;
+    let mut handler: BaseHandler<QuoteValues, QuoteEvent, _> = BaseHandler::new(infer);
     series.for_each_msg(&mut handler).await;
 
-    // predict.run().await?;
+    handler.proc.output_results();
 
     Ok(())
 }
 
 struct Infer {
-    logger: Box<dyn Logger>,
     inferer: Inferer<TheBackend>,
     store: KVStore,
     count: usize,
-    max: usize,
+    run_count: usize,
     lookup: HashMap<EventId,LabelType>,
 }
 
 impl Infer {
-    async fn new(store: KVStore, max: usize, logger: Box<dyn Logger>) -> anyhow::Result<Self> {
+    async fn new(store: KVStore, start_offset: OffsetId, run_count: usize) -> anyhow::Result<Self> {
         let infer = make_inferer()?;
-        let lookup = Self::load_lookup(&store, max).await?;
-        Ok(Infer { logger, inferer: infer, store, count: 0, max, lookup })
+        let lookup = Self::load_lookup(&store, start_offset, run_count).await?;
+        Ok(Infer { inferer: infer, store, count: 0, run_count, lookup })
     }
 
-    async fn load_lookup(store: &KVStore, count: usize) -> anyhow::Result<HashMap<EventId,LabelType>> {
-        let rows = store.label_lookup(START_OFFSET_ID, count).await?;
-        // select event_id, label from ml_demo.labeled where version=1 and offset_from >= 1089327 order by event_id ASC limit 10 allow filtering;
-        // , offset_from, offset_to
+    async fn load_lookup(store: &KVStore, start_offset: OffsetId, count: usize) -> anyhow::Result<HashMap<EventId,LabelType>> {
+        let rows = store.label_lookup(start_offset, count).await?;
         let mut map = HashMap::new();
         for row in rows {
             println!("Added lookup for offset {}", row.offset_from);
@@ -69,33 +75,6 @@ impl Infer {
         println!("keys: {:?}", map.keys());
         Ok(map)
     }
-
-    // async fn run(&self) -> anyhow::Result<()> {
-
-        // let handler = BaseHandler::new(self);
-        // let mut test = Test {};
-        // self.series.for_each_msg(&mut test).await;
-
-            // if !self.handler.is_done() {
-            //     // I think this can never happen. Maybe put in type system?
-            //     println!("This shouldn't happen. Ran out of messages before labelling? Aborting.");
-            //     return Ok(())
-            // }
-
-            // println!("Ended at {:?}", self.handler.ids());
-            // self.store_result().await?;
-
-    //     Ok(())
-    // }
-
-    // async fn on_event(&self, event: Event) {
-    //     println!("Processing event {:?}", event);
-    //     // TODO: buffer
-    //     let inf = Inferred { id: event.id, timestamp: shared_types::now(), inference: Inference::default() };
-    //     let _ = self.store.write_inference(&inf).await.map_err(|e| {
-    //         self.logger.log(format!("Error {} inserting inferred {:?}", e, &inf));
-    //     });
-    // }
 
     async fn store_result(&self) -> anyhow::Result<()> {
         // let timestamp = now();
@@ -119,46 +98,53 @@ impl Infer {
     fn handle_series(&mut self, events: &mut VecDeque<QuoteEvent>) -> bool {
         let event_id = events.back().unwrap().event_id;
 
-        let input = match series_to_input(events) {
+        let input_raw = match series_to_input(events) {
             Ok(input) => input,
             Err(e) => {
                 println!("Failed to convert series to input for event_id {}. Aborting. {:?}", event_id, e);
                 return false;
             },
         };
+        let input = inputs_to_device(vec![input_raw], &self.inferer.device);
 
         match self.lookup.get(&event_id) {
-            Some(label) => {
-                let _ = self.inferer.infer_and_check(input, *label, true);
+            Some(label_raw) => {
+                let expected = outputs_to_device(vec![*label_raw], &self.inferer.device);
+                self.inferer.infer_and_check(input, expected, true);
             },
             None => {
-                match self.inferer.infer_1(input) {
-                    Ok(output) => {
-                        println!("No lookup found for event_id {}, output: {:?}", event_id, output);
-                    },
-                    Err(e) => {
-                        println!("Inference failed for event_id {}. Aborting. {:?}", event_id, e);
-                        return false;
-                    }
-                }
+                let output = self.inferer.infer_batch(input);
+                println!("No lookup found for event_id {}, output: {:?}", event_id, output);
+                    // Ok(output) => {
+                    //     println!("No lookup found for event_id {}, output: {:?}", event_id, output);
+                    // },
+                    // Err(e) => {
+                    //     println!("Inference failed for event_id {}. Aborting. {:?}", event_id, e);
+                    //     return false;
+                    // }
             }
-        };
+        }
 
         self.count += 1;
-        if self.count >= self.max {
+        if self.count >= self.run_count {
             println!("TODO: debug stopping at event_id: {}", event_id);
             false
         } else {
             true
         }
     }
+
+    fn output_results(&self) {
+        println!("Results: {:#?}", self.inferer.metrics);
+        self.inferer.metrics.print_summary();
+    }
 }
 
 impl Processor<VecDeque<QuoteEvent>,QuoteValues> for Infer {
     fn process(&mut self, _start_values: &QuoteValues, events: &mut VecDeque<QuoteEvent>) -> bool {
-        if events.len() == SERIES_SIZE {
+        if events.len() == SERIES1_SIZE {
             let first = &events[0];
-            let last = &events[SERIES_SIZE-1];
+            let last = &events[SERIES1_SIZE-1];
             println!("Found series from offsets {} to {}, event_id: {}", first.offset, last.offset, last.event_id);
             let result = self.handle_series(events);
             let _ = events.pop_front();
